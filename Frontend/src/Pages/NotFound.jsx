9e01d7e1a785e335f17cb4973566f9735f5a8b5a
Tosif2230 <techie.tosif@gmail.com>
2026-01-11 22:38:36 +0530

    Impliment NotFound page for error routes

---
+++ Frontend/src/Pages/NotFound.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+
+function NotFound() {
+  let err = useRouteError();
+  console.log(err.data);
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+      <div className="flex">
+        <img
+        className="justify-center items-center"
+        src="https://www.gstatic.com/youtube/src/web/htdocs/img/monkey.png"
+        alt="monkey"
+      />
+      <h1 className="justify-center text-5xl  text-red-600">{err.status}</h1>
+      </div>
+      <p className="text-[17px] text-gray-700">This page isn't available. Sorry about that.</p>
+      <p className="text-[17px] text-gray-700">Try searching for something else.</p>
+
+      <Link to="/"><h1 className="flex items-center text-[22px] font-semibold">
+        <img
+          src="https://www.youtube.com/s/desktop/12b1ec61/img/favicon_32x32.png"
+          alt="YouTube"
+          className="h-8 w-auto"
+        />
+        YouTube Clone
+      </h1></Link>
+    </div>
+  );
+}
+
+export default NotFound;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,32 +1,33 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { appStore } from './utils/appStore.js'
-import { Provider } from 'react-redux'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { appStore } from "./utils/appStore.js";
+import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { lazy, Suspense } from 'react'
+import { lazy, Suspense } from "react";
+import NotFound from "./Pages/NotFound.jsx";
 
 //Lezy Lodaing
-const Home = lazy(()=> import('./Pages/Home.jsx') )   
+const Home = lazy(() => import("./Pages/Home.jsx"));
 
 const routes = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
         path: "/",
-        element: <App />,
-        children:[
-            {
-                path:"/",
-                element: <Home />
-            }
-        ]
-    }
+        element: <Home />,
+      },
+    ],
+    errorElement: <NotFound />,
+  },
+]);
 
-])
-
-createRoot(document.getElementById('root')).render(
-    <Provider store={appStore} >
-        <Suspense fallback={<p>Loading...</p>}>
-            <RouterProvider router = {routes}/>
-        </Suspense>
-    </Provider>
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={appStore}>
+    <Suspense fallback={<p>Loading...</p>}>
+      <RouterProvider router={routes} />
+    </Suspense>
+  </Provider>
+);
