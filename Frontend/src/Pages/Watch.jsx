a655f2c53eb67468c6e16b894e48bfa482f19fa4
Tosif2230 <techie.tosif@gmail.com>
2026-01-14 17:42:14 +0530

    Add description,comment and add comment section

--- Frontend/src/Pages/Watch.jsx
+++ Frontend/src/Pages/Watch.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { videosData } from "../utils/mockVideos";
-import { AiOutlineLike ,AiOutlineDislike } from "react-icons/ai";
+import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
 import { RiShareForwardLine } from "react-icons/ri";
-
-
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 function Watch() {
   const { id } = useParams();
@@ -17,7 +16,7 @@ function Watch() {
   }
 
   return (
-    <div className="flex gap-20">
+    <div className="flex gap-5">
       {/* LEFT: MAIN VIDEO */}
       <div className="w-[70%]">
         <video controls autoPlay className="w-full h-115 bg-black rounded-xl">
@@ -25,10 +24,6 @@ function Watch() {
         </video>
 
         <h1 className="text-lg font-bold mt-4">{video.title}</h1>
-
-        <p className="text-sm text-gray-600">
-          {video.views.toLocaleString()} views
-        </p>
 
         {/* CHANNEL + ACTIONS */}
         <div className="flex justify-between items-center mt-4">
@@ -47,31 +42,90 @@ function Watch() {
           {/* Like / Dislike */}
           <div className="flex gap-2">
             <button className="px-3 py-1 bg-gray-200 rounded-full">
-             <span className="flex items-center gap-2"> <AiOutlineLike /> {video.likes.toLocaleString()}</span>
+              <span className="flex items-center gap-2">
+                {" "}
+                <AiOutlineLike /> {video.likes.toLocaleString()}
+              </span>
             </button>
             <button className="px-3 py-1 bg-gray-200 rounded-full">
-             <span className="flex items-center gap-2"> <AiOutlineDislike /> {video.dislikes.toLocaleString()}</span>
+              <span className="flex items-center gap-2">
+                {" "}
+                <AiOutlineDislike /> {video.dislikes.toLocaleString()}
+              </span>
             </button>
             <button className="px-3 py-1 bg-gray-200 rounded-full">
-              <span className="flex items-center gap-2">Share <RiShareForwardLine /></span>
+              <span className="flex items-center gap-2">
+                Share <RiShareForwardLine />
+              </span>
             </button>
+          </div>
+        </div>
+
+        {/* DESCRIPTION */}
+        <div className="bg-gray-100 p-3 rounded-xl mt-4">
+          <span className="flex gap-5 font-extrabold">
+            <p className="text-sm text-gray-600">
+              {video.views.toLocaleString()} views
+            </p>
+            <p className="text-sm text-gray-600">
+              {video.createdAt.toLocaleString()}
+            </p>
+          </span>
+          <p className="text-sm mt-3">{video.description}</p>
+        </div>
+
+        {/* COMMENTS */}
+        <div className="mt-6">
+          <h2 className="font-semibold mb-3">
+            {video.comments.length} Comments
+          </h2>
+
+          {/* Add Comment */}
+          <input
+            type="text"
+            placeholder="Add a comment..."
+            className="w-full outline-none pb-2"
+          />
+          <hr className="text-gray-300" />
+
+          {/* Comment List */}
+          <div className="mt-4 space-y-4">
+            {video.comments.map((comment) => (
+              <div
+                key={comment.commentId}
+                className="flex items-start justify-between gap-3"
+              >
+                <div className="flex gap-3">
+                  <div className="w-8 h-8 rounded-full bg-gray-300" />
+
+                  <div>
+                    <p className="text-sm font-semibold">{comment.userId}</p>
+                    <p className="text-sm text-gray-700">{comment.text}</p>
+                  </div>
+                </div>
+
+                <BsThreeDotsVertical className="text-xl text-gray-500 cursor-pointer" />
+              </div>
+            ))}
           </div>
         </div>
       </div>
 
       {/* RIGHT: SUGGESTED VIDEOS */}
-      <div className="w-[20%] space-y-3">
+      <div className="w-[25%] space-y-3">
         {suggestedVideos.map((video) => (
-          <div key={video._id} className="cursor-pointer">
+          <div key={video._id} className="flex gap-2 cursor-pointer">
             <img
               src={video.thumbnailUrl}
               alt={video.title}
-              className="w-full rounded-lg"
+              className="w-35 rounded-lg"
             />
-            <h3 className="text-sm font-semibold line-clamp-2 mt-1">
-              {video.title}
-            </h3>
-            <p className="text-xs text-gray-600">{video.uploader.user}</p>
+            <span>
+              <h3 className="text-sm font-semibold line-clamp-2 mt-1">
+                {video.title}
+              </h3>
+              <p className="text-xs text-gray-600">{video.uploader.user}</p>
+            </span>
           </div>
         ))}
       </div>
